eba201cc5a7f5b1169686688b5e0ace556874ef2
Sameer Bhayani <sbhayani92@gmail.com>
2022-12-31 12:37:29 +0530

    Added service class to remove product from category

---
+++ Services/RemoveProductsFromCategories.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @copyright Copyright (c) sbdevblog (http://www.sbdevblog.com)
+ */
+namespace SbDevBlog\Catalog\Services;
+
+use Magento\Catalog\Model\CategoryLinkRepository;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\StateException;
+use Psr\Log\LoggerInterface;
+
+class RemoveProductsFromCategories
+{
+    /**
+     * @var CategoryLinkRepository
+     */
+    private CategoryLinkRepository $categoryLinkRepository;
+
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    /**
+     * Constructor For Service
+     *
+     * @param CategoryLinkRepository $categoryLinkRepository,
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        CategoryLinkRepository $categoryLinkRepository,
+        LoggerInterface $logger
+    ){
+        $this->categoryLinkRepository = $categoryLinkRepository;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Remove Product From Category
+     *
+     * @param int $categoryId
+     * @param string $sku
+     *
+     * @return bool
+     */
+    public function removeProductsFromCategory(int $categoryId, string $sku):bool
+    {
+        try { 
+                $this->categoryLinkRepository->deleteByIds($categoryId, $sku);
+                return true;
+            } catch (CouldNotSaveException|InputException|StateException $e) {
+                $this->logger->error($e->getMessage());
+            }
+        }
+        return false;
+    }
+}
