549bed653c0be35f215855f96affb9a9a6197507
Sameer Bhayani <sbhayani92@gmail.com>
2023-01-14 15:47:05 +0530

    Create PricingService.php

---
+++ Services/PricingService.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @copyright Copyright (c) sbdevblog (http://www.sbdevblog.com)
+ */
+namespace SbDevBlog\Catalog\Services;
+
+use Magento\Framework\Pricing\Helper\Data as PricingHelper;
+
+class PricingService
+{
+
+  /**
+   * @var PricingHelper
+   */
+  protected PricingHelper $priceHelper;
+  
+  /**
+   * Constructor
+   *
+   * @param PricingHelper $priceHelper
+   */
+  public function __construct(PricingHelper $priceHelper)
+  {
+      $this->priceHelper = $priceHelper;
+  }
+  
+  /**
+   * Get Formatted Price
+   *
+   * @var float $price
+   *
+   * @return string
+   */
+  public function getFormattedPrice(float $price):string
+  {
+      return $this->priceHelper->currency($price, true, false);
+  }
+}
